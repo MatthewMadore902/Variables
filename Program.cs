be2a4bc074f18738fc466ff3f633a6f68ba977aa
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-09-20 12:55:53 -0300

    Added showcase of strings

--- Program.cs
+++ Program.cs
@@ -10,9 +10,35 @@ namespace variables
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World");
-			Console.ReadKey(true);
-			
+			Console.WriteLine("Variables");
+
+			// variable :
+
+
+			// -------------------integer---------------
+
+			int score; // can store whole numbers
+			score = 100; // assigned a integer to score
+
+
+			Console.WriteLine(score); // accessing aid variables
+			Console.WriteLine("Score");
+
+			score = score + 100;
+
+			Console.WriteLine(score);
+
+			//----------------------String-------------------
+
+			string name;
+			name = "NameNameName"; // setting string variable 
+			Console.WriteLine(name);
+			name = "NoName";
+			Console.WriteLine(name);
+
+
+			Console.ReadKey(true); // used for seeing key presses before closing 
+
 		}
 	}
 }
